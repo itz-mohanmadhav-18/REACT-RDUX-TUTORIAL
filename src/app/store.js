82e8d46e0488ecf91564cf9c1cab3a5f86aed8f0
itz-mohanmadhav-18 <mohanmadhav448@outlook.com>
2024-12-30 12:06:27 +0530

    initialised project

---
+++ src/app/store.js
@@ -0,0 +1,12 @@
+
+import { configureStore } from '@reduxjs/toolkit';
+// import your reducers here
+
+const store = configureStore({
+  reducer: {
+    // Add your reducers here
+  },
+});
+
+export default store;
+      
